dff637920931c4388b43c6ef4aef8092e545e091
tontsa28 <xdman7917@gmail.com>
2021-08-08 15:18:19 +0300

    Created UIManager script for managing menu keybindings

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ResetScene : MonoBehaviour
+public class UIManager : MonoBehaviour
 {
     public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
